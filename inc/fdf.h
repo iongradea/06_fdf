929d5168c19bdc99f4c67a8c0be362748ade0675
Ion GRADEA <igradea@e1r4p6.42.fr>
2017-10-24 12:33:04 +0200

    mv_libft_fdf_h

--- inc/fdf.h
+++ inc/fdf.h
@@ -6,8 +6,38 @@
 /*   By: igradea <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/24 11:23:25 by igradea           #+#    #+#             */
-/*   Updated: 2017/10/24 11:41:01 by igradea          ###   ########.fr       */
+/*   Updated: 2017/10/24 12:28:44 by igradea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FDF_H
+# define FDF_H
 
+# include "../libft/libft.h"
+# include "../libft/get_next_line.h"
+# include "../mlx/mlx.h"
+# include <math.h>
+# include <fcntl.h>
+
+# define BUFF_SIZE 1500
+
+typedef struct		s_point
+{
+	float			x;
+	float			y;
+	float			z;
+}					t_point;
+
+typedef struct		s_param
+{
+	void			*mlx;
+	void			*win;
+}					t_param;
+
+typedef struct		s_map
+{
+	int				line_size;
+	int				nb_points;
+}					t_map;
+
+#endif
